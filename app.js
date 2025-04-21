965d0fc717e25c44dbb20be7a4f3dad96e1dd729
AndreiPhoenix <145689886+AndreiPhoenix@users.noreply.github.com>
2025-04-21 23:40:02 +0500

    Update app.js

--- app.js
+++ app.js
@@ -163,3 +163,16 @@ document.addEventListener('DOMContentLoaded', () => {
         }, 3000);
     }
 });
+
+// В начало файла app.js добавить:
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/service-worker.js')
+            .then(registration => {
+                console.log('ServiceWorker registration successful');
+            })
+            .catch(err => {
+                console.log('ServiceWorker registration failed: ', err);
+            });
+    });
+}
